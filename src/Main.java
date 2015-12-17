67613c621d2030fdd6b3a3653a4f232e6b3d8653
Ajordat <ls30687@salleurl.edu>
2015-12-18 00:17:48 +0100

    Afegida GUI, funciona per sudokus quadrats de qualsevol mida.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,47 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Número de valors per recuadre: ");
 		
+		int cas = sc.nextInt();
+		int[][] matriu = new int[cas*cas][cas*cas];
+		boolean[][] fixes = new boolean[cas*cas][cas*cas];
+		
+		String a;
+		String[] aux;
+		
+		BufferedReader br = new BufferedReader(new FileReader("Sudoku.txt"));
+		a = br.readLine();
+		for(int i=0;a!=null;i++){
+			aux = a.split(" ");
+			for(int j=0;j<aux.length;j++){
+				if(aux[j].charAt(0)=='-'){
+					matriu[i][j] = -1;
+					fixes[i][j] = false;
+				}
+				else{
+					fixes[i][j] = true;
+					matriu[i][j] = Integer.parseInt(aux[j]);
+					
+				}
+			}
+			a = br.readLine();
+		}
+		br.close();
+		
+		Sudoku sudoku = new Sudoku(matriu, fixes, cas);
+		SudokuGUI gui = new SudokuGUI("Sudoku", 0, 0, sudoku.getFixes());
+		gui.updateBoard(sudoku.getMatriu());
+		sudoku.resolSudoku(0, 0, gui);
+		gui.updateBoard(sudoku.getMatriu());
 	}
 
 }
--- src/Sudoku.java
+++ src/Sudoku.java
@@ -40,36 +40,48 @@ public class Sudoku {
 	public void setCas(int cas) {
 		this.cas = cas;
 	}
+	
 	private boolean bona(int i, int j){
 		int a = 0, b = 0;
-		for(int x=0; x<=cas*cas;x++) if(matriu[x][j] == matriu[i][j] && x!=i) return false;
-		for(int x=0; x<=cas*cas;x++) if(matriu[i][x] == matriu[i][j] && x!=j) return false;
-		
+		int indx = 0, indy = 0;
+		for(int x=0; x<cas*cas;x++) if(matriu[x][j] == matriu[i][j] && x!=i) return false;
+		for(int x=0; x<cas*cas;x++) if(matriu[i][x] == matriu[i][j] && x!=j) return false;
+		while(indy+cas<=i) indy+=cas;
+		while(indx+cas<=j) indx+=cas;
+		for(a=indy;a<indy+cas;a++){
+			for(b=indx;b<indx+cas;b++){
+				if(matriu[a][b]==matriu[i][j]&&i!=a&&j!=b) return false;
+			}
+		}
 		return true;
 	}
-	public void resolSudoku(int i, int j){
+	public void resolSudoku(int i, int j, SudokuGUI gui){
 		int x = 1;
 		while(x<=cas*cas){
+			
 			if(!fixes[i][j]) matriu[i][j]=x;
 			if(i==cas*cas-1 && j==cas*cas-1){ 
 				if(this.bona(i, j)){ 
-				//	printaSolucio(matriu);
+					SudokuGUI guis = new SudokuGUI("Sudoku", 0, 0, fixes);
+					guis.updateBoard(matriu);
 				}				
 			}
 			else{
 				if(this.bona(i, j)){
+					
 					if(j==cas*cas-1){
-						this.resolSudoku(i+1, 0);
+						gui.updateBoard(matriu);
+						this.resolSudoku(i+1, 0, gui);
 					}
 					else{
-						this.resolSudoku(i, j+1);
+						this.resolSudoku(i, j+1, gui);
 					}
 				}
 			}
 			if(!fixes[i][j]) x++;
 			else x = cas*cas+1;
 		}
-		if(!fixes[i][j]) matriu[i][j]=0;
+		if(!fixes[i][j]) matriu[i][j]=-1;
 	}
 }
 
